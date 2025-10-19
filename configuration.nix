e31261f3920da1508262d561a8de51bf8bcf2d2e
sohail <sohailraj.satapathy@gmail.com>
2025-10-19 11:21:54 +0530

    niri with caelestia set up

---
+++ configuration.nix
@@ -0,0 +1,178 @@
+{ config, pkgs, system, ... }:
+
+{
+  # Nix configuration
+  nix.package = pkgs.nix;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+ 
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  nixpkgs.config.allowUnfree = true;
+
+  # Networking
+  networking.hostName = "sohail-nixos";
+  networking.networkmanager.enable = true;
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+  services.cloudflare-warp.enable = true;
+
+  virtualisation.docker.enable = true;
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = [ "docker0" "wlo1" ];
+    allowedUDPPorts = [ 67 68 53 ];
+    allowedTCPPorts = [ 53 ];
+  };
+
+  hardware.bluetooth = {
+  	enable = true;
+	powerOnBoot = true;
+	settings = {
+		General = {
+			Enable = "Source,Sink,Media,Socket";
+			Experimental = true;
+		};
+	};
+  };
+
+  services.blueman.enable = true;
+  services.power-profiles-daemon.enable = true;
+
+  # Time and locale
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_IN";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Mount additional filesystem
+  fileSystems."/mnt/hdd" = {
+    device = "/dev/disk/by-uuid/f602b400-d48c-4cf4-8aeb-bef6e71cecc5";
+    fsType = "ext4";
+  };
+
+  systemd.tmpfiles.rules = [
+    "z /mnt/hdd 0755 sohail users -"
+  ];
+
+  # Display manager
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  programs.hyprland = {
+  enable = true;
+  xwayland.enable = true; # Needed for apps that don’t support Wayland
+  };
+  
+  environment.sessionVariables = {
+  NIXOS_OZONE_WL = "1"; # Makes Electron apps run natively on Wayland
+  WLR_NO_HARDWARE_CURSORS = "1"; # Fixes black cursor on some GPUs
+  };
+
+  # Enable niri compositor
+  programs.niri.enable = true;
+
+  # Printing
+  services.printing.enable = true;
+  services.flatpak.enable = true;
+
+  # Sound
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+
+    extraConfig.pipewire = {
+    	"context.properties" = {
+		"default.clock.rate" = 48000;
+	};
+    };
+  };
+
+  # User configuration
+  users.users.sohail = {
+    isNormalUser = true;
+    description = "sohail";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+  };
+
+  services.udisks2.enable = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    kitty
+    neovim
+    distrobox
+    firefox
+    bluez
+    bluez-tools
+    brightnessctl
+    pavucontrol
+    upower
+    xwayland
+    adwaita-icon-theme
+    linux-wifi-hotspot
+    iw
+    iwd
+
+    vscode
+    python3
+    xwayland-satellite
+  ];
+
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+      
+      bluez_monitor.rules = {
+        {
+          matches = {
+            {
+              { "device.name", "matches", "bluez_card.*" },
+            },
+          },
+          apply_properties = {
+            ["bluez5.auto-connect"] = "[ a2dp_sink ]",
+            ["bluez5.codec"] = "aac",
+          },
+        },
+      }
+    '';
+  };
+
+  programs.zsh.enable = true;
+
+  services.upower = {
+  	enable = true;
+  };
+
+  system.stateVersion = "25.05";
+}
---
+++ home.nix
@@ -0,0 +1,167 @@
+{ config, pkgs, pkgsUnstable, inputs, ... }:
+{
+  home.username = "sohail";
+  home.homeDirectory = "/home/sohail";
+  
+  # Import Caelestia shell home manager module
+  imports = [
+    inputs.caelestia-shell.homeManagerModules.default
+  ];
+  
+  # Configure Caelestia shell
+  programs.caelestia = {
+    enable = true;
+    
+    systemd = {
+      enable = false;  # We'll start it from niri instead
+      target = "graphical-session.target";
+    };
+    
+    settings = {
+      bar.status = {
+        showBattery = true;  # Set to false if you're on desktop
+      };
+      paths = {
+        wallpaperDir = "~/Pictures/Wallpapers";
+      };
+    };
+    
+    cli = {
+      enable = true;  # Enable CLI for full functionality
+      settings = {
+        theme = {
+          enableGtk = true;
+        };
+      };
+    };
+  };
+  
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+    shellAliases = {
+      ll = "ls -lah";
+      gs = "git status";
+      nv = "nvim";
+      cls = "clear";
+    };
+    initExtra = ''
+      PROMPT="%F{cyan}%n@%m%f:%F{yellow}%~%f$ "
+    '';
+  };
+  
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  
+  programs.git = {
+    enable = true;
+    userName = "Sohail103";
+    userEmail = "sohailraj.satapathy@gmail.com";
+    extraConfig = {
+      core.editor = "nvim";
+    };
+  };
+  
+  home.packages = with pkgs; [
+    neovim
+    kitty
+    wget
+    fastfetch
+    distrobox
+    obsidian
+    cloudflare-warp
+    lazygit
+    unzip
+    luarocks
+    zoxide
+    niri
+    
+    # Wayland utilities
+    fuzzel  # Application launcher (fallback)
+    grim  # Screenshot utility
+    slurp  # Screen area selector
+    wl-clipboard  # Clipboard utilities
+    swaylock  # Screen locker
+    wlogout  # Logout menu
+    brightnessctl  # Brightness control
+    pavucontrol  # Audio control GUI
+    nautilus
+    spotify
+    evince
+    vlc
+    gimp
+
+    # GTK themes (add these)
+    gnome-themes-extra  # For Adwaita-dark
+    adwaita-icon-theme  # For cursor theme
+    libreoffice
+    kicad
+
+    yazi
+    foliate
+    yt-dlp
+    stm32cubemx
+  ];
+  
+  # GTK Configuration - ADD THIS SECTION
+  gtk = {
+    enable = true;
+    
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+    
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+  };
+  
+  # Cursor Configuration - ADD THIS SECTION
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 24;
+    gtk.enable = true;
+  };
+  
+  xdg.desktopEntries.spotify = {
+    name = "Spotify";
+    genericName = "Music Player";
+    exec = "spotify --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+    terminal = false;
+    categories = [ "Audio" "Music" "Player" "AudioVideo" ];
+    mimeType = [ "x-scheme-handler/spotify" ];
+    icon = "spotify-client";
+  };
+
+  xdg.desktopEntries.obsidian = {
+    name = "Obsidian";
+    genericName = "Obsidian";
+    exec = "obsidian --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+    terminal = false;
+    categories = [ "Office" "TextEditor" ];
+    mimeType = [ "x-scheme-handler/obsidian" ];
+    icon = "obsidian";
+  };
+  
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    SHELL = "${pkgs.zsh}/bin/zsh";
+    GTK_THEME = "Adwaita-dark";  # Add this
+  };
+  
+  home.stateVersion = "25.05";
+}
